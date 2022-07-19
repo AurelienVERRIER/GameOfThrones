baef0e36441bd4901e4045d09b77cf28f958a331
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-07-19 17:05:30 +0200

    First exercise done

---
+++ got/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor () {
+    super ()
+    this.state = {
+      characters: []
+    }
+  }
+
+  async componentDidMount () {
+    const request = await fetch("https://thronesapi.com/api/v2/Characters");
+    console.log("request", request);
+    const readableArray = await request.json();
+    console.log("readableArray", readableArray);
+    this.setState({
+      characters: readableArray
+    })
+    console.log("this.state.characters", this.state.characters);
+  }
+
+  
+
+	render() {
+		return(
+			<h1>Game of thrones</h1>
+		)
+	}
+}
+
+export default App
