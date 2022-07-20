bf8c341f28e5283b94aafa294ae8dc56ea9a970c
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-07-20 12:19:31 +0200

    full display (name, title & image) working

--- got/src/App.js
+++ got/src/App.js
@@ -5,28 +5,41 @@ class App extends React.Component {
   constructor () {
     super ()
     this.state = {
-      characters: []
+      characters: [],
+      name: ""
     }
   }
 
   async componentDidMount () {
     const request = await fetch("https://thronesapi.com/api/v2/Characters");
-    console.log("request", request);
+    // console.log("request", request);
     const readableArray = await request.json();
-    console.log("readableArray", readableArray);
+    // console.log("readableArray", readableArray);
     this.setState({
       characters: readableArray
     })
-    console.log("this.state.characters", this.state.characters);
+    // console.log("this.state.characters", this.state.characters);
+  
   }
 
   
 
 	render() {
+    console.log(this.state.characters)
 		return(
       <>
 			<h1>Game of thrones</h1>
-      <Character/>
+      
+      {/* {console.log("this.state", this.state)} */}
+      
+      {this.state.characters.map((character) => (
+        
+        <Character 
+          fullName = {character.fullName}
+          title = {character.title}
+          image = {character.imageUrl}
+        />
+      ))}
       </>
     )
 	}
--- got/src/components/Character.js
+++ got/src/components/Character.js
@@ -3,8 +3,12 @@ import React from 'react'
 class Character extends React.Component {
 	render() {
 		return(
-			<p>Character</p>
-		)
+			<>
+				<h2>{this.props.fullName}</h2>
+				<h3>{this.props.title}</h3>
+				<img src={this.props.image} />
+			</>
+			)
 	}
 }
 
